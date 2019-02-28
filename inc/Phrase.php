ff576cab40c2ce36a201f375a3159d0e4592446b
Christina <thisluckygrrl@gmail.com>
2019-02-27 20:12:28 -0800

    Add files via upload
    changes to play.php

--- inc/Phrase.php
+++ inc/Phrase.php
@@ -18,44 +18,35 @@ class Phrase
     } elseif (isset($currentPhrase)) {
         $this->currentPhrase = $phrase;
     }*/
-  }
 
-  if (!isset($currentPhrase)) {
-    $this->currentPhrase = $phrase;
+
+  if (isset($currentPhrase)) {
+    $this->currentPhrase = $currentPhrase;
   } else {
     $this->currentPhrase = 'Dream Big';
   }
 
-  if (!isset($selected)) {
+  if (isset($selected)) {
     $this->selected = $selected;
 
   }
+}
 
 
-
-
+/*
   function setPhrase($currentPhrase) {
     if (!isset($currentPhrase)) {
         $this->currentPhrase = 'Dream Big';
     }  else {
         $this->currentPhrase = $phrase;
     }
-  }
+  }*/
 
   function getPhrase() {
       return $this->currentPhrase;
   }
 
-  function addPhraseToDisplay() {
-      //Split string into lowercase characters
-      $characters = str_split(strtolower($this->phrase));
-      //loop through characters and display as <li>
-      //reference: http://thisinterestsme.com/php-loop-characters-string/
-      foreach ($characters as $character) {
-          //if(isset($character)) {
-            //echo $character . "<br>";
-            echo "<li class='hide letter'>" . $character . "</li>";
-          //}
-  }
-}
+
+    }
+
 }
--- play.php
+++ play.php
@@ -11,27 +11,38 @@ $game = new Game($phrase);
 echo $phrase->addPhraseToDisplay();
 
 //var_dump for testing classes
-var_dump($phrase);
+var_dump($phrase->getPhrase());
 var_dump($game);
 
 ?>
-
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Phrase Hunter</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/styles.css" rel="stylesheet">
-    <link href="css/animate.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-</head>
+	<head>
+		<meta charset="utf-8">
+		<title>Phrase Hunter</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="css/styles.css" rel="stylesheet">
+		<link href="css/animate.css" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	</head>
 
-<body>
-<div class="main-container">
-    <div id="banner" class="section">
-        <h2 class="header">Phrase Hunter</h2>
-    </div>
-</div>
-
-</body>
+	<body>
+		<div class="main-container">
+			<h2 class="header">Phrase Hunter</h2>
+      <?php
+      function addPhraseToDisplay() {
+          //Split string into lowercase characters
+          $characters = str_split(strtolower($this->currentPhrase));
+          //loop through characters and display as <li>
+          //reference: http://thisinterestsme.com/php-loop-characters-string/
+          foreach ($characters as $character) {
+              if(empty($character)) {
+                //echo $character . "<br>";
+                echo "<li class='hide space'>" . $character . "</li>";
+              } else {
+                echo "<li class='hide letter'>" . $character . "</li>";
+              }
+          }
+          ?>
+		</div>
+	</body>
 </html>
